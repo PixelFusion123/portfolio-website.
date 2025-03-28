e6bb72cb7fd94706704052c2ce04130b436ee96d
PixelFusion123 <pxlfsn@gmail.com>
2025-03-28 19:39:36 +0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to my animated portfolio! 🚀");
+});
+
+document.querySelector("button").addEventListener("click", function () {
+    alert("Thanks for reaching out! I'll get back to you soon.");
+});
